3f7cd2e60ebd4687f0c12fd6e14e464d632ff255
Mae Moon <maemoon@proton.me>
2026-01-30 21:11:29 -0700

    Menu music

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -29,3 +29,7 @@ func _on_credits_button_pressed() -> void:
 
 func _on_back_button_pressed() -> void:
 	switch_screen("main")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("back"):
+		switch_screen("main")
