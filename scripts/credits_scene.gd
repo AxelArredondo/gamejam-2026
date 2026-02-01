fc7159fcb2ab29458348d9966d403be01d3719b9
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2026-02-01 14:29:59 -0700

    credits
    credits

--- scripts/credits_scene.gd
+++ scripts/credits_scene.gd
@@ -3,6 +3,42 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	var text_box1 = RichTextLabel.new()
+	text_box1.text = "
+	Credits
+	
+Cam
+Jordi 
+Rain Vasquez
+Taran Hurley
+Tim 
+Mae
+Conner 
+Autum
+Lemon
+	
+Game Design/programming
+Axel Arredondo
+Alex Ilnytsky
+Rain Vaquez
+Connor Bosco
+Autum Darell
+Mae Moon
+
+Art
+Camille Tsu
+Jordy Usen
+Alma
+
+Music
+Tim Rivero
+Jordan Parm
+	"
+	text_box1.bbcode_enabled = true # Enable BBCode for rich formatting
+	text_box1.custom_minimum_size = Vector2(110, 300) # Set minimum size
+	add_child(text_box1)
+	text_box1.position = Vector2(120, 10)
 	pass # Replace with function body.
 
 
