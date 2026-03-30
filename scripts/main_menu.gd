015a917b5eb59c9ea048e9f30856fc4517b58693
Bones <terraw1999@proton.me>
2026-03-29 19:31:24 -0600

    Fix scenes filepath

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -31,4 +31,4 @@ func _on_back_button_pressed() -> void:
 	switch_screen("main")
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/cave.tscn")
+	get_tree().change_scene_to_file("res://scenes/cave.tscn")
